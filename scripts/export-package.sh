df2c9f5fa6a745937e9cdf90d2fcf8d48904b096
Andrew Lord <andrewlord1990@gmail.com>
2016-12-11 15:09:48 +0000

    Updated the export package script so that it supports having all your packages within company directory, then have different flavours of the package. I.e. library with multiple packages for different parts.

--- scripts/export-package.sh
+++ scripts/export-package.sh
@@ -17,11 +17,13 @@
 # limitations under the License.
 ################################################################################
 
-project=$1
+company=$1
+project=$2
+package=$3
 project_path=$(pwd)/src/$project
 log_file=$(pwd)/build/unity-mac.log
 export_directory=$(pwd)/current-package
-export_path=$export_directory/$project.unitypackage
+export_path=$export_directory/$project.$package.unitypackage
 error_code=0
 
 mkdir -p $export_directory
@@ -33,7 +35,7 @@ echo "Creating package."
   -silent-crashes \
   -logFile "$log_file" \
   -projectPath "$project_path" \
-  -exportPackage "Assets/$project" "$export_path" \
+  -exportPackage "Assets/$company/$project/$package" "$export_path" \
   -quit
 if [ $? = 0 ] ; then
   echo "Created package successfully."
